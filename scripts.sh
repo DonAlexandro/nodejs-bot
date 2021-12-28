5f7a150812de91ee45ed158ffff259bae36203d4
Tanasienko Dima <Tanasienko.Dmitrii@ericom.com>
2021-12-28 10:28:04 +0200

    add more scripts

--- scripts.sh
+++ scripts.sh
@@ -11,20 +11,28 @@ bot-dev-down() {
 }
 
 bot-prod-start() {
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-}
-
-bot-prod-build() {
   if [ ! -f .env ]; then
     read -p "Please enter Telegram bot token: " token
     echo "TOKEN=$token" > .env
   fi
 
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+}
+
+bot-prod-build() {
+  docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
 }
 
 bot-prod-down() {
   docker-compose -f docker-compose.yml -f docker-compose.prod.yml down -v
+}
+
+bot-prod-push() {
+  docker-compose -f docker-compose.yml -f docker-compose.prod.yml push bot
+}
+
+bot-prod-pull() {
+  docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
 }
 
 
